cae5ac95833ef7de29cc5a43b11790c84f941d54
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2023-04-20 09:52:29 +0530

    Add equivalant axioms and quantifiers axioms to the ontology

--- src/main/java/org/ontobot/OntoBuilder.java
+++ src/main/java/org/ontobot/OntoBuilder.java
@@ -1,5 +1,6 @@
 package org.ontobot;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -36,7 +37,8 @@ public class OntoBuilder {
         this.ontology = this.manager.createOntology(this.ontologyIRI);
     }
 
-    public void build(String[] concepts, JsonArray taxonomies, JsonArray... ops){
+    public void build(String[] concepts, JsonArray taxonomies){
+
         try {
             // Define concepts
             for (String concept : concepts) {
@@ -51,6 +53,7 @@ public class OntoBuilder {
             for (JsonElement taxonomy: taxonomies) {
                 JsonObject classObject = taxonomy.getAsJsonObject();
                 String className = classObject.get("class_name").getAsString(); // superClass
+                JsonElement equal_className = classObject.get("equal_class_name");
                 String level = classObject.get("level").getAsString();
                 JsonArray attributes = classObject.get("attributes").getAsJsonArray();
                 JsonArray disjointConcepts = classObject.get("disjoint").getAsJsonArray();
@@ -62,6 +65,8 @@ public class OntoBuilder {
                     // Create the RDFS comment annotation
                     this.defineConceptComments(className, level);
 
+                    // define equal concepts
+                    this.defineEqualConcept(equal_className, className);
 
                     for (JsonElement attr : attributes) {
                         JsonObject attrObj = attr.getAsJsonObject();
@@ -82,6 +87,7 @@ public class OntoBuilder {
                     for (JsonElement subClass: subClasses) {
                         JsonObject subClassObject = subClass.getAsJsonObject();
                         String subClassName = subClassObject.get("class_name").getAsString(); // subClass
+                        JsonElement sub_equal_className = subClassObject.get("equal_class_name");
                         String subLevel = subClassObject.get("level").getAsString();
                         JsonArray subAttributes = subClassObject.get("attributes").getAsJsonArray();
                         OWLClass subClazz = this.hashMap.get(subClassName);
@@ -93,6 +99,154 @@ public class OntoBuilder {
                             // Create the RDFS comment annotation
                             this.defineConceptComments(subClassName, subLevel);
 
+                            // define equal concepts
+                            this.defineEqualConcept(sub_equal_className, subClassName);
+
+                            for (JsonElement attr : subAttributes) {
+                                JsonObject attrObj = attr.getAsJsonObject();
+                                String name = attrObj.get("name").getAsString();
+                                String type = attrObj.get("datatype").getAsString().toLowerCase();
+                                boolean isFunctional = attrObj.get("functional").getAsBoolean();
+
+                                OWLDataProperty dataProperty = dataFactory.getOWLDataProperty(IRI.create(this.ontologyIRI + "#" + name.replace(" ", "_")));
+                                defineDataProperty(dataProperty, subClassName, isFunctional, type);
+
+                            }
+                        }
+                        this.addedConcepts.add(subClassName);
+                    }
+
+                    this.addedConcepts.add(className);
+                }
+
+                // set disjoint properties
+                if (disjointConcepts.size() > 0){
+                    List<OWLClassExpression> disjointList = new ArrayList<>();
+                    for (JsonElement disjointSet: disjointConcepts) {
+                        if (disjointSet.isJsonArray()) {
+                            JsonArray jsonArray = disjointSet.getAsJsonArray(); // convert JsonElement to JsonArray
+
+                            String[] stringArray = new String[jsonArray.size()]; // create new String array with same size as JsonArray
+
+                            for (int i = 0; i < jsonArray.size(); i++) {
+                                stringArray[i] = jsonArray.get(i).getAsString(); // convert each JsonElement to String and add to string array
+                                disjointList.add(this.hashMap.get(stringArray[i]));
+                            }
+
+                            if (disjointList.size() > 0){
+                                // create the disjoint classes axiom
+                                OWLDisjointClassesAxiom axiom = this.dataFactory.getOWLDisjointClassesAxiom(disjointList);
+                                // add the axiom to the ontology
+                                manager.addAxiom(ontology, axiom);
+                            }
+
+                            disjointList.clear();
+
+                        }
+                    }
+                }
+
+                // set overlap properties
+                if (overlapConcepts.size() > 0){
+                    List<OWLClassExpression> overlapList = new ArrayList<>();
+
+                    for (JsonElement overlapSet: overlapConcepts){
+                        if (overlapSet.isJsonArray()){
+                            JsonArray jsonArray = overlapSet.getAsJsonArray();
+
+                            String[] stringArray = new String[jsonArray.size()]; // create new String array with same size as JsonArray
+
+                            for (int i = 0; i < jsonArray.size(); i++) {
+                                stringArray[i] = jsonArray.get(i).getAsString(); // convert each JsonElement to String and add to string array
+                                overlapList.add(this.hashMap.get(stringArray[i]));
+                            }
+
+                            if (overlapList.size() > 0){
+                                // Create the union class
+                                OWLClass unionClass = this.dataFactory.getOWLClass(IRI.create(this.ontologyIRI + "#" + "Union" + Integer.toString(seqNumber))); seqNumber++;
+                                // Create the disjoint union axiom
+                                OWLDisjointUnionAxiom axiom = this.dataFactory.getOWLDisjointUnionAxiom(unionClass, overlapList);
+                                manager.addAxiom(ontology, axiom);
+                            }
+
+                            overlapList.clear();
+                        }
+                    }
+
+
+                }
+
+            }
+
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+
+    public void build(String[] concepts, JsonArray taxonomies, JsonArray ops){
+        try {
+            // Define concepts
+
+            for (String concept : concepts) {
+                String class_name = concept.substring(1, concept.length() - 1);
+                System.out.println(class_name);
+                OWLClass clazz = this.dataFactory.getOWLClass(IRI.create(this.ontologyIRI + "#" + class_name.replace(" ", "_")));
+                this.hashMap.put(class_name, clazz);
+                this.manager.addAxiom(this.ontology, this.dataFactory.getOWLDeclarationAxiom(clazz));
+            }
+
+            // Define Taxonomies with Data properties
+            for (JsonElement taxonomy: taxonomies) {
+                JsonObject classObject = taxonomy.getAsJsonObject();
+                String className = classObject.get("class_name").getAsString(); // superClass
+                JsonElement equal_className = classObject.get("equal_class_name");
+                String level = classObject.get("level").getAsString();
+                JsonArray attributes = classObject.get("attributes").getAsJsonArray();
+                JsonArray disjointConcepts = classObject.get("disjoint").getAsJsonArray();
+                JsonArray overlapConcepts = classObject.get("overlap").getAsJsonArray();
+
+                // set superclass data properties
+                if (!addedConcepts.contains(className)){
+                    // define comment
+                    // Create the RDFS comment annotation
+                    this.defineConceptComments(className, level);
+
+                    // define equal concepts
+                    this.defineEqualConcept(equal_className, className);
+
+                    for (JsonElement attr : attributes) {
+                        JsonObject attrObj = attr.getAsJsonObject();
+                        String name = attrObj.get("name").getAsString();
+                        String type = attrObj.get("datatype").getAsString().toLowerCase();
+                        boolean isFunctional = attrObj.get("functional").getAsBoolean();
+
+                        OWLDataProperty dataProperty = this.dataFactory.getOWLDataProperty(IRI.create(this.ontologyIRI + "#" + name.replace(" ", "_")));
+                        defineDataProperty(dataProperty, className, isFunctional, type);
+
+                    }
+                }
+
+
+
+                if (classObject.has("sub_classes")){
+                    JsonArray subClasses = classObject.get("sub_classes").getAsJsonArray();
+                    for (JsonElement subClass: subClasses) {
+                        JsonObject subClassObject = subClass.getAsJsonObject();
+                        String subClassName = subClassObject.get("class_name").getAsString(); // subClass
+                        JsonElement sub_equal_className = subClassObject.get("equal_class_name");
+                        String subLevel = subClassObject.get("level").getAsString();
+                        JsonArray subAttributes = subClassObject.get("attributes").getAsJsonArray();
+                        OWLClass subClazz = this.hashMap.get(subClassName);
+                        OWLClass supClazz = this.hashMap.get(className);
+                        this.manager.addAxiom(this.ontology, this.dataFactory.getOWLSubClassOfAxiom(subClazz, supClazz));
+
+                        if (!addedConcepts.contains(subClassName)){
+                            // define comment
+                            // Create the RDFS comment annotation
+                            this.defineConceptComments(subClassName, subLevel);
+
+                            // define equal concepts
+                            this.defineEqualConcept(sub_equal_className, subClassName);
 
                             for (JsonElement attr : subAttributes) {
                                 JsonObject attrObj = attr.getAsJsonObject();
@@ -171,8 +325,8 @@ public class OntoBuilder {
             }
 
             // Define Object properties
-            if (ops.length > 0){
-                for (JsonElement op: ops[0]) {
+            if (ops.size() > 0){
+                for (JsonElement op: ops) {
                     JsonObject opObject = op.getAsJsonObject();
                     String propertyName = opObject.get("op_name").getAsString();
                     String inversePropertyName = opObject.get("op_inverse").getAsString();
@@ -208,6 +362,18 @@ public class OntoBuilder {
                         // add the axiom to the ontology
                         this.manager.applyChanges(new AddAxiom(this.ontology, axiom));
                     }
+                    if (quantifier.get("min").getAsInt() != 0){
+                        // Define min cardinality restriction on prop for Class1
+                        OWLClassExpression minCardinality = this.dataFactory.getOWLObjectMinCardinality(quantifier.get("min").getAsInt(), property, rangeClass);
+                        OWLSubClassOfAxiom minCardinalityAxiom = this.dataFactory.getOWLSubClassOfAxiom(rangeClass, minCardinality);
+                        this.manager.addAxiom(this.ontology, minCardinalityAxiom);
+                    }
+                    if (!Objects.equals(quantifier.get("max").getAsString(), "inf")){
+                        // Define max cardinality restriction on prop for Class2
+                        OWLClassExpression maxCardinality = this.dataFactory.getOWLObjectMaxCardinality(quantifier.get("max").getAsInt(), property, rangeClass);
+                        OWLSubClassOfAxiom maxCardinalityAxiom = this.dataFactory.getOWLSubClassOfAxiom(rangeClass, maxCardinality);
+                        this.manager.addAxiom(this.ontology, maxCardinalityAxiom);
+                    }
 
 
                     // add comments
@@ -226,6 +392,14 @@ public class OntoBuilder {
                         OWLAxiom labelAxiom = this.dataFactory.getOWLAnnotationAssertionAxiom(property.getIRI(), labelAnnotation);
                         OWLAxiom commentAxiom = this.dataFactory.getOWLAnnotationAssertionAxiom(property.getIRI(), commentAnnotation);
                         manager.applyChanges(Arrays.asList(new AddAxiom(ontology, labelAxiom), new AddAxiom(ontology, commentAxiom)));
+
+                        // Get references to the object properties
+                        OWLObjectProperty equalProperty = this.dataFactory.getOWLObjectProperty(IRI.create(this.ontologyIRI + "#" + comment.replace(" ", "_")));
+                        manager.addAxiom(this.ontology, this.dataFactory.getOWLDeclarationAxiom(equalProperty));
+                        // Define an equivalent object property axiom
+                        OWLEquivalentObjectPropertiesAxiom equivAxiom = this.dataFactory.getOWLEquivalentObjectPropertiesAxiom(property, equalProperty);
+                        // Add the axiom to the ontology
+                        manager.addAxiom(ontology, equivAxiom);
                     }
 
                     // define inverse property
@@ -311,6 +485,38 @@ public class OntoBuilder {
 
         OWLAxiom commentAxiom = this.dataFactory.getOWLAnnotationAssertionAxiom(this.hashMap.get(className).getIRI(), commentAnnotation);
         this.manager.applyChange(new AddAxiom(this.ontology, commentAxiom));
+    }
+
+    private void defineEqualConcept(JsonElement equal_className, String className){
+        Gson gson = new Gson();
+        Object equal_class = gson.fromJson(equal_className, Object.class);
+        if (equal_class instanceof String){
+            String copy_ec_name = (String) equal_class;
+            if (copy_ec_name.length() > 0){
+                OWLClass clazz = this.dataFactory.getOWLClass(IRI.create(this.ontologyIRI + "#" + copy_ec_name.replace(" ", "_")));
+                this.manager.addAxiom(this.ontology, this.dataFactory.getOWLDeclarationAxiom(clazz));
+                // Define an equivalent classes axiom
+                OWLEquivalentClassesAxiom equivAxiom = this.dataFactory.getOWLEquivalentClassesAxiom(this.hashMap.get(className), clazz);
+                // Add the axiom to the ontology
+                this.manager.addAxiom(ontology, equivAxiom);
+            }
+        }
+        else {
+            String[] strArray = (String[]) equal_class;
+            OWLClass[] classes = new OWLClass[strArray.length+1];
+            classes[0] = this.hashMap.get(className);
+            int index = 1;
+            for (String ec_name: strArray) {
+                OWLClass clazz = this.dataFactory.getOWLClass(IRI.create(this.ontologyIRI + "#" + ec_name.replace(" ", "_")));
+                this.manager.addAxiom(this.ontology, this.dataFactory.getOWLDeclarationAxiom(clazz));
+                classes[index] = clazz;
+                index ++;
+            }
+            // Define an equivalent classes axiom
+            OWLEquivalentClassesAxiom equivAxiom = this.dataFactory.getOWLEquivalentClassesAxiom(classes);
+            // Add the axiom to the ontology
+            this.manager.addAxiom(this.ontology, equivAxiom);
+        }
     }
 
 
